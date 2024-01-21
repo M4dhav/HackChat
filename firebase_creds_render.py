f71e229bfea53d86a191f632be94f67646f4a72a
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2024-01-21 12:41:02 +0530

    Update firebase_creds_render.py

--- firebase_creds_render.py
+++ firebase_creds_render.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
-print(type.os.environ.get('project_id'))
+print(type(os.environ.get('project_id')))
 if not firebase_admin._apps:
     cred = credentials.Certificate({\
         "type": "service_account",\
